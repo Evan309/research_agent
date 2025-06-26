a959b45e6d2518300c003af2d485b7614c286c97
Evan Shi <eshi061272@gmail.com>
2025-06-25 17:43:43 -0700

    gitignore, env added / llm client initialized - groq

--- backend/app/llm/llm_client.py
+++ backend/app/llm/llm_client.py
@@ -1,1 +1,28 @@
 # use groq with llama3-70b-8192
+import logging
+from groq import GroqClient
+
+# initialize logging
+logger = logging.getLogger(__name__)
+
+class LLMClient:
+    def __init__(self, api_key: str, model: str = "llama3-70b-8192"):
+        self.api_key = api_key
+        self.model = model
+        self.client = GroqClient(api_key=api_key)
+        logger.info(f"initialized LLMClient with model: {self.model}")
+
+
+    def generate_response(self, prompt: str, max_tokens: int, temperature: float = 0.7) -> str:
+
+        # gerate a response using the Groq client
+        logger.info(f"Generating response for prompt: {prompt} with max_tokens: {max_tokens}")
+        response = self.client.chat.completions.create(
+            model = self.model,
+            messages = [{"role": "user", "content": prompt}],
+            max_tokens = max_tokens,
+            tempreature = temperature
+        )
+
+        logger.info(f"Response generated: {response.choices[0].message.content}")
+        return response.choices[0].message.content
---
+++ backend/app/llm/prompts.py
@@ -0,0 +1,7 @@
+TOPIC_CLASSIFICATION_PROMPT = """
+Extract the main topic or subject from the following user query.  
+Respond with only a few keywords (no explanation).
+
+Query: "{query}"
+Topic:
+"""
