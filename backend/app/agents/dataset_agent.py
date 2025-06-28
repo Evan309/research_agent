42bec0e5f2fb255ea36333c8de406103e6cf7614
Evan Shi <eshi061272@gmail.com>
2025-06-27 17:40:43 -0700

    dataset agent started

---
+++ backend/app/agents/dataset_agent.py
@@ -0,0 +1,13 @@
+# serach datasets from the web based on the topic given by the task planner
+class DatasetAgent:
+    def __init__(self):
+        pass
+
+    def search_datasets(self, topic: str) -> list[dict]:
+        pass
+    
+    def get_dataset_info(self, dataset_id: str):
+        pass
+    
+    def get_dataset_files(self, dataset_id: str):
+        pass
