523c3f10da360d58e01368da157bbfc6dcc1f276
Evan Shi <eshi061272@gmail.com>
2025-06-30 15:44:00 -0700

    kaggle api implemented - untested

--- backend/app/agents/dataset_agent.py
+++ backend/app/agents/dataset_agent.py
@@ -1,10 +1,17 @@
-# serach datasets from the web based on the topic given by the task planner
+import logging
+from kaggle.api.kaggle_api_extended import KaggleApi
+
+# initialize logging
+logger = logging.getLogger(__name__)
+
 class DatasetAgent:
     def __init__(self):
-        pass
+        self.kaggle_api = KaggleApi()
+        self.kaggle_api.authenticate()
 
     def search_kaggle_datasets(self, topic: str) -> list[dict]:
-        pass
+        results = self.kaggle_api.datasets.search_datasets(query=topic)
+        return results
 
     def search_huggingface_datasets(self, topic: str) -> list[dict]:
         pass
