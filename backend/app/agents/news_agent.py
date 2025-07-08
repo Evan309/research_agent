1cd37905aa3b54259833ba4a674b48f1d5ad2593
Evan Shi <eshi061272@gmail.com>
2025-07-08 11:24:51 -0700

    news search filters added

--- backend/app/agents/news_agent.py
+++ backend/app/agents/news_agent.py
@@ -1,15 +1,23 @@
 # search news agents and return the latest news about the research topic
 import logging
+import os
+import requests
+from dotenv import load_dotenv
 
 # initialize logging
 logger = logging.getLogger(__name__)
+
+# load environment variables
+load_dotenv()
 
 
 class NewsAgent:
     def __init__(self, embedder):
         self.embedder = embedder
+        self.GNEWS_API_KEY = os.getenv("GNEWS_API_KEY")
 
-    def search_news(self, topic: str):
+
+    def search_news(self, topic: str, max: int = 10, sortby: str = "publishedAt"):
         pass
 
     def summarize_news_article(self, article_url: str):
